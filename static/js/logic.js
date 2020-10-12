41ead995a6ee0e76fa6c39fc06c94aef685a3108
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-12 10:38:34 -0400

    Update logic.js

---
+++ static/js/logic.js
@@ -0,0 +1,4 @@
+var quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+console.log (quakes)
+var plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+console.log (plates)
