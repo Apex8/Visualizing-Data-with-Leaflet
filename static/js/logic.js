e0997b652518b503cb6a174316c861e6b862e23c
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-12 10:42:25 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,3 +2,15 @@ var quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_mont
 console.log (quakes)
 var plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 console.log (plates)
+
+function markerSize(magnitude) {
+    return magnitude * 5;
+};
+
+var earthquakes = new L.LayerGroup();
+
+d3.json(API_quakes, function (geoJson) {
+    L.geoJSON(geoJson.features, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            return L.circleMarker(latlng, { radius: markerSize(geoJsonPoint.properties.mag) });
+        },
