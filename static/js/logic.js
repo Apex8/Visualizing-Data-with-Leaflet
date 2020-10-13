1a0a67ca46d4a0b7aafd72b0aae4a0cd0a98c111
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-13 18:20:12 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,8 +7,7 @@ d3.json(quakes, function(data) {
 
   function createFeatures(quakeData) {
     function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        layer.popup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
       }
 
       function radiusSize(magnitude) {
